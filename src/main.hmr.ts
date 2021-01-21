21f16d7b0614b60bfad7d3fab53cb1a1ed2dbb00
exlineo <gabbadie@step-sa.fr>
2021-01-21 22:59:53 +0100

    Modifiation des paramètres Wbpack (hmr.ts)

---
+++ src/main.hmr.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import * as bodyParser from 'body-parser';
+
+import { AppModule } from './app.module';
+// import { DispatchError } from './shared/filters/dispatch-error';
+
+declare const module: any;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: true,
+      preflightContinue: false,
+    }
+  });
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  // app.enableCors();
+  // app.useGlobalFilters(new DispatchError());
+
+  await app.listen(8080);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+}
+bootstrap();
