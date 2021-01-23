d7d2d2b7a85001aac7424a62e5a948403efb02d9
exlineo <gabbadie@step-sa.fr>
2021-01-23 12:36:21 +0100

    Configuration again

--- src/config.ts
+++
@@ -1,2 +0,0 @@
-export const SERV_ADR = 'mongodb://admin:exlineo@localhost:27017/?authSource=admin'; // Connexion à MongoDB
-export const JWT_CONFIG = {secret: "CollectionsNemateria", expiration: 36000000};
